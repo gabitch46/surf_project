18fcf409bd08da5d1647ec796ba0b7819d55b58c
gabitch46 <gabyprojet96@gmail.com>
2018-10-01 11:44:22 +0200

    Complete intialisation script

--- intialisation_projet.sh
+++ intialisation_projet.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 #django-admin startproject surf_project
-#cd surf_project/
+export PATH="~/anaconda3/bin:$PATH"
+source activate projet_surf
+cd surf_project/
 python manage.py runserver
